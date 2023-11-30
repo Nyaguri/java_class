bb8d35eac14c6cf75018852780e1327094dd4df4
nyaguri <tsuki4026@gmail.com>
2023-11-30 15:48:39 +0900

    산술연산 추가

---
+++ src/ch01_Variables/Ex01_ArithmeticOperator.java
@@ -0,0 +1,21 @@
+package ch01_Variables;
+
+public class Ex01_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// 산술연산자
+		/**
+		 * num1, num2 두개의 정수형 변수를 선언하고 값은 마음대로
+		 * 두 변수의 +, -, *, /, % 의 연산결과를 print()를 활용하여 출력해봅시다
+		 * 출력방법
+		 * System.out.println(num1 + num2);
+		 */
+		int num1 = 11, num2 = 30;
+		System.out.println("덧셈 결과: " + (num1 + num2));
+		System.out.println("뺄셈 결과: " + (num1 - num2));
+		System.out.println("곱셈 결과: " + (num1 * num2));
+		System.out.println("나눗셈 결과: " + (num1 / num2));
+		System.out.println("나머지 결과: " + (num1 % num2));
+	}
+
+}
