3b19e8db543cc58db99c996f1356ca9343c0e172
nyaguri <tsuki4026@gmail.com>
2023-12-11 08:56:14 +0900

    배열 예제 수정

--- src/ch09_array/Ex09_ArrayEx.java
+++ src/ch09_array/Ex09_ArrayEx.java
@@ -11,7 +11,7 @@ public class Ex09_ArrayEx {
         int[] stu = null;
         int[] result = null;
         int result1 = 0; // 점수
-        int avg; // 평균 점수
+        double avg; // 평균 점수
         int sum = 0; // 합계
         int temp;
         int max = -2147483647; // 최고 점수
@@ -35,15 +35,14 @@ public class Ex09_ArrayEx {
                     System.out.print((i + 1) + "번 학생 점수 : ");
                     result1 = scanner.nextInt();
                     result[i] = result1;
+                    sum += result[i];
                 }
             } else if (serv == 3) {
                 for (int j = 0; j < result.length; j++) {
                     System.out.println(j + 1 + "번 학생 점수 : " + result[j]);
-                    System.out.println(result[j]);
                 }
             } else if (serv == 4) {
                 for (int i = 0; i < result.length; i++) {
-                    sum += result[i];
                     for (int j = i + 1; j < result.length; j++) {
                         if (result[i] < result[j]) {
                             temp = result[i];
@@ -53,10 +52,15 @@ public class Ex09_ArrayEx {
                     }
                     System.out.println((i+1)+"등 : " + result[i]);
                 }
-                avg = (double)(sum / Objects.requireNonNull(stu).length;
+                avg = ((double) sum / Objects.requireNonNull(stu).length);
                 System.out.println("최고 점수는 : " + result[0]);
                 System.out.println("총합 : " + sum);
                 System.out.println("평균 : "+ avg);
+            }else if (serv == 5){
+                System.out.println("종료합니다.");
+                break;
+            }else {
+                System.out.println("다시 입력해 주세요");
             }
         }
     }
